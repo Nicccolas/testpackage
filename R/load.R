4fe1e47e5f64e2c1249d68c8700f9a4bf178477c
Nicolas Zhu <nicolas_zhu@icloud.com>
2025-04-22 15:54:04 -0700

    test

--- R/load.R
+++ R/load.R
@@ -1,13 +1,25 @@
+#' load titanic.csv
+#'
+#' @param file_path the data
+#' @param output_path the output directory
+#'
+#' @return the cleaned data
+#' @export
+#'
+#' @examples
+#' \dontrun{
+#' load("data/raw/titanic.csv", "output/cleaned.csv")
+#' }
 load <- function(file_path, output_path) {
   # load data
-  data <- read_csv(file_path)
+  data <- readr::read_csv(file_path)
 
   # clean data
   data <- janitor::clean_names(data)
 
   data
 
-  write_csv(data, output_path)
+  readr::write_csv(data, output_path)
 
   print("finished script 1")
 
